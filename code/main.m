c9ae0b668867404067da376d1ce95cf825ec5be1
PattyBG <patrick.bordes01@gmail.com>
2016-12-30 22:08:36 +0100

    added slam_map, which enables to plot the map at the end of an iteration

--- code/main.m
+++ code/main.m
@@ -2,7 +2,7 @@ m=3; %number of features
 n=2*m+2; %dimension of the state vector
 % Dimension = 2
 
-nb_iterations = 10; %number of iterations
+nb_iterations = 1; %number of iterations
 K = 10; %number of iterations for the computation of the mean estimate
 
 %Initialization
@@ -23,10 +23,13 @@ Z = 0.01;
 
 %Iteration
 for k = 1:nb_iterations
+    
     display(k)
+    
     [H_t_bar,b_t_bar,mu_t_bar] = SEIF_motion_update(previous_H_t,previous_b_t,previous_mu_t,u_t,A_t,U_t,n);
     
     [z_t_hat,id] = robot.measure_closest(env_features); 
+    
     mu_t = SEIF_state_estimation_2(H_t_bar,b_t_bar,mu_t_bar,K,id,n);
     
     % the noise for measurement is included here. 
@@ -37,7 +40,13 @@ for k = 1:nb_iterations
    
     [H_t_tild,b_t_tild] = SEIF_sparsification(H_t,b_t,mu_t,n);
 
+    robot.x_position = mu_t(1); %update robot position
+    robot.y_position = mu_t(2);
+    
     previous_H_t = H_t_tild;
     previous_b_t = b_t_tild;
     previous_mu_t = mu_t;
+    
+    %plots the map (estimated robot position, estimated features, real position of features)
+    slam_map(mu_t,env_features);  
 end
---
+++ code/slam_map.m
@@ -0,0 +1,37 @@
+function y= slam_map(mu,env_features)
+
+n = size(mu,1);
+m = floor(n/2-1); %number of features
+
+X_features_real = zeros(m,1);
+Y_features_real = zeros(m,1);
+
+for k=0:(m-1)
+    X_features_real(k+1) = env_features(2*k+1);
+    Y_features_real(k+1) = env_features(2*k+2);
+end
+
+robot_position_x = mu(1);
+robot_position_y = mu(2);
+
+X_features_estimated = zeros(m,1);
+Y_features_estimated = zeros(m,1);
+
+for k=1:m
+    X_features_estimated(k) = mu(2*k+1);
+    Y_features_estimated(k) = mu(2*k+2);
+end
+
+X = vertcat(X_features_estimated,X_features_real);
+Y = vertcat(Y_features_estimated,Y_features_real);
+
+scatter(X_features_estimated,Y_features_estimated,500,'d');
+hold on
+scatter(X_features_real,Y_features_real,140,'MarkerEdgeColor',[0 .5 .5],...
+              'MarkerFaceColor',[0 .7 .7],...
+              'LineWidth',1.5)
+hold on
+scatter(robot_position_x,robot_position_y,'filled','o')
+legend('estimated features','real features','robot position');
+
+end
